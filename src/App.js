65bbb613598032c8d174315d58d1d02704852063
Stefan <me@Stefan.com>
2019-08-03 22:27:48 +0300

    setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar/Navbar';
+
+class App extends Component {
+  render() {
+    return <Navbar />
+  }
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+	return (
+		<nav>
+			<h2>Where in the world?</h2>
+			<p><i className="far fa-moon"></i> Dark Mode</p>
+		</nav>
+	)
+}	
+
+export default Navbar;
