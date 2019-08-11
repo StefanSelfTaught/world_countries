4d4bbc5c5f6aa91329a295db453953da48de3eb1
Stefan <me@Stefan.com>
2019-08-11 19:44:27 +0300

    Updates

---
+++ precache-manifest.451b19605f91d16149043f284accf36c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25abd5671931ec4821769b80fec207d",
+    "url": "/StefanSelfTaught/world_countries/index.html"
+  },
+  {
+    "revision": "03bb428bf21b7e303c37",
+    "url": "/StefanSelfTaught/world_countries/static/css/main.b69ba5e0.chunk.css"
+  },
+  {
+    "revision": "e4b0f3e845feb52d147e",
+    "url": "/StefanSelfTaught/world_countries/static/js/2.2a2c25a0.chunk.js"
+  },
+  {
+    "revision": "03bb428bf21b7e303c37",
+    "url": "/StefanSelfTaught/world_countries/static/js/main.38640e03.chunk.js"
+  },
+  {
+    "revision": "5c0b40b989c202b878c3",
+    "url": "/StefanSelfTaught/world_countries/static/js/runtime~main.83d2c025.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/StefanSelfTaught/world_countries/precache-manifest.451b19605f91d16149043f284accf36c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/StefanSelfTaught/world_countries/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
