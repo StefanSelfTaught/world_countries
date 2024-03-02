ed823d3b4dc08d2cba28b2c85744593a3953356e
Stefan Pop <43994418+StefanSelfTaught@users.noreply.github.com>
2024-03-02 11:01:27 +0000

    Updates

---
+++ precache-manifest.953b18105ce2de062e42bd372bf8ffb5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb04c1a509c06e3ea19e297078ddbc8",
+    "url": "/world_countries/index.html"
+  },
+  {
+    "revision": "b17d550b8c0df48ddf54",
+    "url": "/world_countries/static/css/4.947b90ce.chunk.css"
+  },
+  {
+    "revision": "8074c883b8fe3735d3bb",
+    "url": "/world_countries/static/css/5.d24d1213.chunk.css"
+  },
+  {
+    "revision": "f3f607115ef427d5785a",
+    "url": "/world_countries/static/css/main.a164ff78.chunk.css"
+  },
+  {
+    "revision": "6964c426d7697d1e09e1",
+    "url": "/world_countries/static/js/0.71c719f8.chunk.js"
+  },
+  {
+    "revision": "23a1c7a0a982e176797b",
+    "url": "/world_countries/static/js/3.f544bd69.chunk.js"
+  },
+  {
+    "revision": "b17d550b8c0df48ddf54",
+    "url": "/world_countries/static/js/4.9f0c787c.chunk.js"
+  },
+  {
+    "revision": "8074c883b8fe3735d3bb",
+    "url": "/world_countries/static/js/5.c7a6a54c.chunk.js"
+  },
+  {
+    "revision": "2451524f188781253a08",
+    "url": "/world_countries/static/js/6.1153d3e4.chunk.js"
+  },
+  {
+    "revision": "f3f607115ef427d5785a",
+    "url": "/world_countries/static/js/main.40dd3343.chunk.js"
+  },
+  {
+    "revision": "d40de11eef440c19ff13",
+    "url": "/world_countries/static/js/runtime~main.12beeabd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/world_countries/precache-manifest.256d4cf8cb5ef9d518f97267d0389534.js"
+  "/world_countries/precache-manifest.953b18105ce2de062e42bd372bf8ffb5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/world_countries/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
